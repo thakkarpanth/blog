cd1208f2765827ac9834a65c48e167d11cf43294
thakkarpanth <panth.thakkar@actonate.com>
2021-04-09 09:02:41 +0530

    feat(Comment Create):UI for Comment Create

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import CommentCreate from './CommentCreate';
 
 const PostList = () => {
     const [posts, setPosts] = useState({});
@@ -16,15 +16,16 @@ const PostList = () => {
 
     const renderedPosts = Object.values(posts).map(post => {
         return (
-            <div key = {post.id} className="card" style={{ width: "30%", marginBottom: "20px" }}>
-                <div className = "card-body">
-                    <h3>{post.title}</h3>  
+            <div key={post.id} className="card" style={{ width: "30%", marginBottom: "20px" }}>
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                    <CommentCreate postId={post.id} />
                 </div>
             </div>
         )
     });
     return (
-        <div className = "d-flex flex-row flex-wrap justify-content-between">
+        <div className="d-flex flex-row flex-wrap justify-content-between">
             {renderedPosts}
         </div>
     )
